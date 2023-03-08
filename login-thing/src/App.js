998dd51c291ab01c653643287e7db246f183f979
jfroehli <jfroehli@trinity.edu>
2023-03-08 13:22:24 -0600

    the login is now working, can make accounts to login

--- login-thing/src/App.js
+++ login-thing/src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Amplify} from 'aws-amplify';
+import {Amplify, Auth} from 'aws-amplify';
 import awsconfig from './aws-exports';
-import {AmplifySignOut, withAuthenticator} from '@aws-amplify/ui-react';
+import {Authenticator, withAuthenticator} from '@aws-amplify/ui-react';
 
 Amplify.configure(awsconfig);
+Auth.configure(awsconfig);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <AmplifySignOut />
-        <h2>My App Content</h2>
+      <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
       </header>
     </div>
   );
